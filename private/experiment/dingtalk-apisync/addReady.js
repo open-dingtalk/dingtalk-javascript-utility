01edee59f3062a07cd82893c1738192ba6d4b1fa
icepy <xiangwenwe@foxmail.com>
2017-05-20 02:59:33 +0800

    同步api，提供无ready版本

---
+++ private/experiment/dingtalk-apisync/addReady.js
@@ -0,0 +1,86 @@
+import dingtalk from 'weex-dingtalk';
+import env from 'dingtalk-env';
+import timer from 'dingtalk-timer';
+
+const { setTimeout, clearTimeout } = timer;
+
+function apisync(){
+  let container;
+  let _timeoutStatu = false;
+  let _apis;
+  while(true){
+    var apis = null;
+    let timeoutStatu = false;
+    let once = true;
+    let timeout;
+    if (once){
+      once = false;
+      dingtalk.ready(function(){
+        apis = dingtalk.apis;
+      });
+      timeout = setTimeout(function(){
+        timeoutStatu = true;
+        clearTimeout(timeout);
+      },1000);
+    }
+    if (timeoutStatu){
+      _timeoutStatu = true;
+      throw new Error('sync apis timeout 1000ms');
+      break;
+    }
+    if (apis){
+      _apis = apis;
+      clearTimeout(timeout);
+      break;
+    }
+  }
+  if (!_timeoutStatu && _apis){
+    container = reloadApis(apis);
+  }
+  return container;
+}
+
+function reloadApis(apis){
+  let container = Object.create(null);
+  for (let spaceName in apis){
+    let api = apis[spaceName];
+    if (typeof api === 'function'){
+      container[spaceName] = api;
+    }
+    if (typeof api === 'object' && api !== null){
+      container[spaceName] = recursiveApis(spaceName,api,Object.create(null));
+    }
+  }
+  return container;
+}
+
+function recursiveApis(spaceName,api,c,action){
+  let node = Object.keys(api);
+  let i = 0;
+  let j = node.length;
+  for(;i<j;i++){
+    let apiName = node[i];
+    if (typeof api[apiName] === 'function'){
+      c[apiName] = function(conf){
+        dingtalk.ready(function(){
+          const dd = dingtalk.apis;
+          dd[spaceName][action][apiName](conf);
+        });
+      }
+      continue;
+    }
+    if (typeof api[apiName] === 'object' && api[apiName] !== null){
+      c[apiName] = recursiveApis(spaceName, api[apiName],Object.create(null),apiName);
+    }
+  }
+  return c;
+}
+
+let initStatus = true;
+let Apis = {};
+if (initStatus){
+  initStatus = false;
+  Apis = apisync();
+}
+
+export default Apis;
--- private/experiment/dingtalk-apisync/index.js
+++ private/experiment/dingtalk-apisync/index.js
@@ -1,11 +1,9 @@
 import dingtalk from 'weex-dingtalk';
-import env from 'dingtalk-env';
 import timer from 'dingtalk-timer';
 
 const { setTimeout, clearTimeout } = timer;
 
 function apisync(){
-  let container = Object.create(null);
   let _timeoutStatu = false;
   let _apis;
   while(true){
@@ -21,11 +19,11 @@ function apisync(){
       timeout = setTimeout(function(){
         timeoutStatu = true;
         clearTimeout(timeout);
-      },5000);
+      },1000);
     }
     if (timeoutStatu){
       _timeoutStatu = true;
-      throw new Error('sync apis timeout 5000ms');
+      throw new Error('sync apis timeout 1000ms');
       break;
     }
     if (apis){
@@ -34,49 +32,14 @@ function apisync(){
       break;
     }
   }
-  if (!_timeoutStatu && _apis){
-    container = reloadApis(apis);
-  }
-  return container;
+  return _apis || {};
 }
 
-function reloadApis(apis){
-  let container = Object.create(null);
-  for (let spaceName in apis){
-    let api = apis[spaceName];
-    if (typeof api === 'function'){
-      container[spaceName] = api;
-    }
-    if (typeof api === 'object' && api !== null){
-      container[spaceName] = recursiveApis(spaceName,api,Object.create(null));
-    }
-  }
-  return container;
+let initStatus = true;
+let Apis = {};
+if (initStatus){
+  initStatus = false;
+  Apis = apisync();
 }
 
-function recursiveApis(spaceName,api,c,action){
-  let node = Object.keys(api);
-  let i = 0;
-  let j = node.length;
-  for(;i<j;i++){
-    let apiName = node[i];
-    if (typeof api[apiName] === 'function'){
-      c[apiName] = function(conf){
-        dingtalk.ready(function(){
-          const dd = dingtalk.apis;
-          dd[spaceName][action][apiName](conf);
-        });
-      }
-      continue;
-    }
-    if (typeof api[apiName] === 'object' && api[apiName] !== null){
-      c[apiName] = recursiveApis(spaceName, api[apiName],Object.create(null),apiName);
-    }
-  }
-  return c;
-}
-
-export default function dingtalkApiSync(){
-  const apis = apisync();
-  return apis;
-}
+export default Apis;
